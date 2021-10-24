eddada5bf700a60459015d97f0f35f21b75a4f9b
cristian <crisdarroca@hotmail.com>
2021-10-23 21:04:53 -0500

    add contenido

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ProductList from "../containers/ProductList";
 import Header from "../components/Header";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <ProductList />
     </>
   );
 };
